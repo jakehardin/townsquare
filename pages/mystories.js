280f57f622ace9e1b10cf2bcdd9fac2e832d2510
jakehardin <jakehardin13@gmail.com>
2023-03-06 19:06:13 -0600

    moving on from mystories for now

---
+++ pages/mystories.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getStories } from '../api/storiesData';
+import StoryCard from '../components/StoryCard';
+import { useAuth } from '../utils/context/authContext';
+
+export default function MainPage() {
+  const [stories, setStories] = useState([]);
+  const { user } = useAuth();
+
+  const getAllStories = () => {
+    getStories(user.uid).then(setStories);
+  };
+  useEffect(() => {
+    getAllStories();
+  }, []);
+  return (
+    <div className="text-center my-4">
+      <Link href="/story/new" passHref>
+        <Button>+ Add A Story</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over books here using BookCard component */}
+        {stories.map((story) => (
+          <StoryCard key={story.firebaseKey} storyObj={story} onUpdate={getAllStories} />
+        ))}
+      </div>
+
+    </div>
+  );
+}
