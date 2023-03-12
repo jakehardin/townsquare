d5a1d93dc313d0881ab503213a54e83616b88a99
jakehardin <jakehardin13@gmail.com>
2023-03-11 21:20:21 -0600

    corresponding topic now shows up on story details

--- pages/story/[firebaseKey].js
+++ pages/story/[firebaseKey].js
@@ -1,13 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { viewPostDetails } from '../../api/mergedData';
+import { viewPostDetails, viewStoryTopic } from '../../api/mergedData';
 import CommentCard from '../../components/CommentCard';
 import CommentForm from '../../components/forms/CommentForm';
 import { useAuth } from '../../utils/context/authContext';
 
 export default function ViewPost() {
   const [postDetails, setPostDetails] = useState({});
+  const [topic, setTopic] = useState([]);
   const { user } = useAuth();
   const router = useRouter();
   const { firebaseKey } = router.query;
@@ -19,6 +20,9 @@ export default function ViewPost() {
   useEffect(() => {
     viewPostDetails(firebaseKey).then(setPostDetails);
   }, [firebaseKey]);
+  useEffect(() => {
+    viewStoryTopic(firebaseKey).then(setTopic);
+  }, [firebaseKey]);
 
   return (
     <>
@@ -26,7 +30,7 @@ export default function ViewPost() {
         <div className="d-flex">
           <img src={postDetails.image} alt={postDetails.title} style={{ height: '200px' }} />
           <h5>
-            {postDetails.topic_id}
+            {topic.topicObject?.topic_name}
           </h5>
         </div>
         <div className="text-white ms-5 details">
