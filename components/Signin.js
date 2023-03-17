486162f415c10104a22168359a48bad257848559
jakehardin <jakehardin13@gmail.com>
2023-03-16 19:37:15 -0500

    added picture to sign in

--- components/Signin.js
+++ components/Signin.js
@@ -12,9 +12,11 @@ function Signin() {
         maxWidth: '400px',
         margin: '0 auto',
       }}
+      id="signin"
     >
       <div>
-        <img src="images/townsquare.png" width="75%" height="75%" alt="townsquare" />
+        <img src="https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/9f/26/8d/9f268dd4-327b-b179-215d-3068eb35d5de/source/512x512bb.jpg" width="75%" height="75%" alt="townsquare" />
+        <h1>Welcome to the Town Square!</h1>
       </div>
       <p>Click the button below to login!</p>
       <button type="button" className="btn btn-primary btn-lg copy-btn" onClick={signIn}>
